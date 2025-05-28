23d440b2ff576fde43d06cc7ead50c39c37e9df3
NilNull818 <diaosi.dev@gmail.com>
2025-05-28 09:49:20 +0800

    demo代码提交

---
+++ app1/src/main/java/com/example/myapp1/MainActivity.java
@@ -0,0 +1,76 @@
+package com.example.myapp1;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.myapp1.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button mBt;
+    // com.kx.b 是B授权包名,如果读者Uri调起APP不熟悉,请查阅Uri拉起APP相关知识点,博主有时间的话,会在下篇补上相关知识点
+    private String mUri = "kx://com.kx.bapp2/sign?type=1&user_package_name=com.kx.b";
+    private MyBroadcastReceiver mReceiver;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //操作1 按钮发起
+        mBt = findViewById(R.id.bt);
+        mBt.setOnClickListener(v -> {
+            Intent intent = new Intent();
+            intent.setData(Uri.parse(mUri));//参数拼接在URI后面 type=1是授权页面,user_package_name使用者包名,后续参数可自行添加
+            intent.putExtra("", "");//这里Intent也可传递参数,但是一般情况下都会放到上面的URL中进行传递
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+        });
+
+        //操作2
+        //注册广播接受者,接收授权成功返回广播信息
+        mReceiver = new MyBroadcastReceiver();
+        IntentFilter intentFilter = new IntentFilter();
+        //kx.com.kx.b.sign 自行定义action 即可
+        intentFilter.addAction("kx.com.kx.bapp2.sign");
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            registerReceiver(mReceiver, intentFilter, Context.RECEIVER_NOT_EXPORTED);
+        } else {
+            registerReceiver(mReceiver, intentFilter);
+        }
+    }
+
+
+    //操作3
+    //BroadcastReceiver 接收授权成功返回广播信息,TODO
+    private class MyBroadcastReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            // 授权成功  返回token,app_pkg
+            Toast.makeText(MainActivity.this, "授权成功!", Toast.LENGTH_SHORT).show();
+            final String token = intent.getStringExtra("token");
+            final String app_pkg = intent.getStringExtra("app_pkg");
+            mBt.setText("token=" + token + "\n" + "app_pkg=" + app_pkg);
+
+            //todo 调起方登录操作 做你需要的需求
+        }
+    }
+
+    //别忘了回收广播  不然会报error,这里是一个内存回收的知识点,第二个有时间博主会补上的知识点
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(mReceiver);
+    }
+}
---
+++ app1/src/main/java/com/example/myapp1/SignActivity.java
@@ -0,0 +1,63 @@
+package com.example.myapp1;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.myapp1.R;
+
+public class SignActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button mBt;
+    private String mUser_package_name;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign);
+
+        mBt = findViewById(R.id.bt_confirm_login);
+        mBt.setOnClickListener(this);
+        mBt.setEnabled(false);
+
+        //获取传递的数据
+        Intent intent = getIntent();
+        Uri uri = intent.getData();
+
+        //获取参数值
+        String type = uri.getQueryParameter("type");
+        mUser_package_name = uri.getQueryParameter("user_package_name");
+
+        //类型type 检验
+        if (TextUtils.equals(type, "1")) {
+            //Todo 未登录 处理
+            request5037();
+        }
+    }
+
+    /**
+     * 外部 app 拉起授权 (code:5037)
+     * 服务器校验 是否授权
+     * 授权 页面A状态
+     */
+
+    private void request5037() {
+        mBt.setEnabled(true);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        Intent intent1 = new Intent();
+        intent1.setAction("kx.com.kx.bapp1.sign");
+        intent1.putExtra("token", "xxxx-xxxx-xxxxx");
+        intent1.putExtra("app_pkg", "com.kx.aapp");
+        sendBroadcast(intent1);
+        finish();
+    }
+}
